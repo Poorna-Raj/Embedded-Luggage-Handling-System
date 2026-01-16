52a82cf029e8a1fee9669fb19d651afbf0383bae
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-01-16 21:00:37 +0530

    define IR sensor

---
+++ sorting-system-nibm/include/utils/IrSensor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Arduino.h>
+
+struct IrSensor
+{
+    const uint8_t gpio;
+
+    bool current;
+    bool previous;
+    bool event;
+
+    bool initialized;
+};
+
+IrSensor IrSensor_Create(const uint8_t gpio);
+
+void IrSensor_Init(IrSensor &ir);
+void IrSensor_Update(IrSensor &ir);
+
+bool IrSensor_IsDetected(IrSensor &ir);
+bool IrSensor_IsEventTrue(IrSensor &ir);
