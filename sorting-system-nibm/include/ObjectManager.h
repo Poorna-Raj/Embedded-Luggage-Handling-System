c588c1773816260f8a830759312fcf303ee1a32a
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-01-17 22:40:56 +0530

    define object manager

---
+++ sorting-system-nibm/include/ObjectManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "utils/IrSensor.h"
+#include "utils/ColorSensor.h"
+
+enum class ObjManagerState
+{
+    IDLE,
+    DETECTED,
+    FAILED
+};
+
+struct ObjectManager
+{
+    IrSensor &ir;
+    ColorSensor &cs;
+
+    ObjManagerState state;
+    Color color;
+    Color preColor;
+
+    bool initialized;
+
+    bool detectedPrinted;
+    bool failedPrinted;
+};
+
+ObjectManager ObjectManager_Create(IrSensor &ir, ColorSensor &cs);
+
+void ObjectManager_Init(ObjectManager &omn);
+void ObjectManager_Update(ObjectManager &omn);
+
+bool ObjectManager_IsEventTrue(ObjectManager &omn);
+Color ObjectManager_GetColor(const ObjectManager &omn);
