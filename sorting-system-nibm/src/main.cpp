916c74b2769b6e62c3a7b8979b5c28d59fff2251
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-01-23 20:05:32 +0530

    test the test code for bin manager

--- sorting-system-nibm/src/main.cpp
+++ sorting-system-nibm/src/main.cpp
@@ -1,52 +1,35 @@
 #include <Arduino.h>
-#include "ObjectManager.h"
-#include "SortingManager.h"
-#include "utils/ServoMotor.h"
+#include "BinManager.h"
 
-// -------------------- Object Manager --------------------
-ColorSensor cs = ColorSensor_Create(2, 3, 4); // Pins for color sensor
-IrSensor ir = IrSensor_Create(7);             // Object detection IR
+IrSensor bin1Ir = IrSensor_Create(4);
+IrSensor bin2Ir = IrSensor_Create(5);
+IrSensor bin3Ir = IrSensor_Create(14);
 
-ObjectManager objMng = ObjectManager_Create(ir, cs);
-
-// -------------------- Servo Motors --------------------
-constexpr uint8_t SERVO1_PIN = 9; // Left bin
-constexpr uint8_t RESET1_ANGLE = 30;
-constexpr uint8_t PUSH1_ANGLE = 120;
-
-constexpr uint8_t SERVO2_PIN = 11; // Right bin
-constexpr uint8_t RESET2_ANGLE = 30;
-constexpr uint8_t PUSH2_ANGLE = 120;
-
-ServoMotor redServo;
-ServoMotor greenServo;
-
-// -------------------- Bin IR sensors --------------------
-IrSensor bin1Ir = IrSensor_Create(5);
-IrSensor bin2Ir = IrSensor_Create(10);
-IrSensor bin3Ir = IrSensor_Create(8); // End bin IR
-
-// -------------------- Hardware structs --------------------
-Hardware bin1 = {ir : bin1Ir, servo : &redServo};
-Hardware bin2 = {ir : bin2Ir, servo : &greenServo};
-
-// -------------------- Sorting Manager --------------------
-SortingManager sorter = SortingManager_Create(bin1, bin2, bin3Ir, objMng);
+BinManager binMng = BinManager_Create(bin1Ir, bin2Ir, bin3Ir);
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("Sorting Manager Test Start...");
 
-    ServoMotor_Create(&redServo, SERVO1_PIN, RESET1_ANGLE, PUSH1_ANGLE);
-    ServoMotor_Create(&greenServo, SERVO2_PIN, RESET2_ANGLE, PUSH2_ANGLE);
-
-    ObjectManager_Init(objMng);
-    SortingManager_Init(sorter);
+    BinManager_Init(binMng);
 }
 
 void loop()
 {
-    // -------------------- Update Sorting Manager --------------------
-    SortingManager_Update(sorter);
+    BinManager_Update(binMng);
+    if (BinManager_GetState(binMng) == BinManagerState::FULL)
+    {
+        Serial.println(BinManager_GetBin(binMng) == Bin::BIN_1 ? "1" : BinManager_GetBin(binMng) == Bin::BIN_2 ? "2"
+                                                                   : BinManager_GetBin(binMng) == Bin::BIN_3   ? "3"
+                                                                                                               : "NONE");
+        if (Serial.available())
+        {
+            char ch = Serial.read();
+            if (ch == 'o')
+            {
+                BinManager_OnCompleted(binMng);
+            }
+        }
+    }
 }
