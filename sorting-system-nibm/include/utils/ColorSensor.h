4d4026f872dcf37d3f29d16366fcfea13d01cde7
AroshaBandara09 <flim21038@gmail.com>
2026-01-31 13:39:07 +0530

    update color sensor with calibrator method

--- sorting-system-nibm/include/utils/ColorSensor.h
+++ sorting-system-nibm/include/utils/ColorSensor.h
@@ -1,26 +1,20 @@
 #pragma once
-
 #include <Arduino.h>
 
-enum class Color
-{
-    RED,
-    BLUE,
-    GREEN,
-    NONE
-};
+enum class Color { RED, BLUE, GREEN, NONE };
 
-struct ColorSensor
-{
+struct ColorSensor {
     const uint8_t s2Gpio;
     const uint8_t s3Gpio;
     const uint8_t outGpio;
     int samplesToAverage;
-
     bool initialized;
+
+    // Calibration data
+    int minValues[3]; // Stores the "White" values for R, G, B
 };
 
 ColorSensor ColorSensor_Create(const uint8_t s2, const uint8_t s3, const uint8_t out);
 void ColorSensor_Init(ColorSensor &cs);
-
+void ColorSensor_CalibrateWhite(ColorSensor &cs); // New function
 Color ColorSensor_DetectColor(ColorSensor &cs);
--- sorting-system-nibm/src/utils/ColorSensor.cpp
+++ sorting-system-nibm/src/utils/ColorSensor.cpp
@@ -1,78 +1,86 @@
 #include "utils/ColorSensor.h"
 
-ColorSensor ColorSensor_Create(const uint8_t s2, const uint8_t s3, const uint8_t out)
-{
+ColorSensor ColorSensor_Create(const uint8_t s2, const uint8_t s3, const uint8_t out) {
     return ColorSensor{
         .s2Gpio = s2,
         .s3Gpio = s3,
         .outGpio = out,
         .samplesToAverage = 5,
-        .initialized = false,
+        .initialized = false
     };
 }
 
-void ColorSensor_Init(ColorSensor &cs)
-{
-    if (cs.initialized)
-        return;
+void ColorSensor_Init(ColorSensor &cs) {
+    if (cs.initialized) return;
 
     pinMode(cs.s2Gpio, OUTPUT);
     pinMode(cs.s3Gpio, OUTPUT);
     pinMode(cs.outGpio, INPUT);
 
+    // Initial state
     digitalWrite(cs.s2Gpio, LOW);
     digitalWrite(cs.s3Gpio, LOW);
 
     cs.initialized = true;
 }
 
-int ColorSensor_ReadFreq(ColorSensor &cs, Color color)
-{
-    switch (color)
-    {
-    case Color::RED:
-        digitalWrite(cs.s2Gpio, LOW);
-        digitalWrite(cs.s3Gpio, LOW);
-        break;
-    case Color::GREEN:
-        digitalWrite(cs.s2Gpio, HIGH);
-        digitalWrite(cs.s3Gpio, HIGH);
-        break;
-    case Color::BLUE:
-        digitalWrite(cs.s2Gpio, LOW);
-        digitalWrite(cs.s3Gpio, HIGH);
-        break;
-    default:
-        return -1;
+// Internal helper for reading raw frequency
+static int ColorSensor_ReadFreq(ColorSensor &cs, Color color) {
+    switch (color) {
+        case Color::RED:
+            digitalWrite(cs.s2Gpio, LOW);  digitalWrite(cs.s3Gpio, LOW);
+            break;
+        case Color::GREEN:
+            digitalWrite(cs.s2Gpio, HIGH); digitalWrite(cs.s3Gpio, HIGH);
+            break;
+        case Color::BLUE:
+            digitalWrite(cs.s2Gpio, LOW);  digitalWrite(cs.s3Gpio, HIGH);
+            break;
+        default: return 99999;
     }
 
-    return pulseIn(cs.outGpio, LOW, 1000);
+    // Crucial for ESP8266: Wait for sensor to switch filters
+    delay(10); 
+    
+    // Increased timeout to 20ms for reliable reading on ESP8266
+    long duration = pulseIn(cs.outGpio, LOW, 20000);
+    return (duration == 0) ? 99999 : (int)duration;
 }
 
-Color ColorSensor_DetectColor(ColorSensor &cs)
-{
-    if (!cs.initialized)
+void ColorSensor_CalibrateWhite(ColorSensor &cs) {
+    Serial.println("Calibrating... Place a WHITE object in front of sensor.");
+    delay(2000); // Give user time to place object
+    cs.minValues[0] = ColorSensor_ReadFreq(cs, Color::RED);
+    cs.minValues[1] = ColorSensor_ReadFreq(cs, Color::GREEN);
+    cs.minValues[2] = ColorSensor_ReadFreq(cs, Color::BLUE);
+    Serial.println("Calibration Done!");
+}
+
+Color ColorSensor_DetectColor(ColorSensor &cs) {
+    if (!cs.initialized) return Color::NONE;
+
+    // Get raw pulse widths (Lower = Stronger)
+    int r = ColorSensor_ReadFreq(cs, Color::RED);
+    int g = ColorSensor_ReadFreq(cs, Color::GREEN);
+    int b = ColorSensor_ReadFreq(cs, Color::BLUE);
+
+    // DEBUG PRINT - Look at these numbers in Serial Monitor!
+    Serial.printf("RAW DATA -> R: %d | G: %d | B: %d\n", r, g, b);
+
+    // If G is 99999 or 0, the pin/wire is broken.
+    if (g >= 99999 || g <= 0) {
+        Serial.println("ERROR: Green sensor reading failed. Check S2/S3 wiring.");
         return Color::NONE;
-
-    int redSum = 0, greenSum = 0, blueSum = 0;
-
-    for (int i = 0; i < cs.samplesToAverage; i++)
-    {
-        redSum += ColorSensor_ReadFreq(cs, Color::RED);
-        greenSum += ColorSensor_ReadFreq(cs, Color::GREEN);
-        blueSum += ColorSensor_ReadFreq(cs, Color::BLUE);
     }
 
-    int redAvg = redSum / cs.samplesToAverage;
-    int greenAvg = greenSum / cs.samplesToAverage;
-    int blueAvg = blueSum / cs.samplesToAverage;
+    // Apply a "Green Bias" 
+    // Most TCS3200 sensors are 20-30% less sensitive to Green.
+    // We subtract a buffer from Green to make it "more competitive."
+    int greenAdjusted = g - (g * 0.20); 
 
-    if (redAvg < greenAvg && redAvg < blueAvg)
-        return Color::RED;
-    if (greenAvg < redAvg && greenAvg < blueAvg)
-        return Color::GREEN;
-    if (blueAvg < redAvg && blueAvg < greenAvg)
-        return Color::BLUE;
+    if (r < greenAdjusted && r < b) return Color::RED;
+    if (greenAdjusted < r && greenAdjusted < b) return Color::GREEN;
+    if (b < r && b < greenAdjusted) return Color::BLUE;
 
     return Color::NONE;
 }
